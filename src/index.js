1e70e34a92e8f012462bbfe1907ad70d4ad1065d
David Virtser <david@dorbel.com>
2016-10-25 11:52:34 +0300

    moving monitoring to its own function to prevent errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-// NewRelic init
-process.env.NEW_RELIC_NO_CONFIG_FILE = 'True';
-require('newrelic');
-
 module.exports = {
   config: require('./config'),
   logger: require('./logger'),
+  startMonitor: () => {
+    // NewRelic init 
+    process.env.NEW_RELIC_NO_CONFIG_FILE = 'True'; 
+    require('newrelic'); 
+  },
   middleware: {
     requestLogger: require('./koa-middleware/request-logger'),
     swaggerModelValidator: require('./koa-middleware/swagger-model-validator')

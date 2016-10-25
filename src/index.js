5b22ca670d6f00b51f19b65831614e1c3585d06b
David Virtser <david@dorbel.com>
2016-10-25 14:20:10 +0300

    another try to load new relic without env vas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+// NewRelic init
+if (!process.env.NEW_RELIC_ENABLED) { 
+  process.env.NEW_RELIC_NO_CONFIG_FILE = 'True'; 
+  process.env.NEW_RELIC_ENABLED = 'False'
+}
+require('newrelic'); 
+
 module.exports = {
   config: require('./config'),
   logger: require('./logger'),
-  startMonitor: () => {
-    // NewRelic init 
-    process.env.NEW_RELIC_NO_CONFIG_FILE = 'True'; 
-    require('newrelic'); 
-  },
   middleware: {
     requestLogger: require('./koa-middleware/request-logger'),
     swaggerModelValidator: require('./koa-middleware/swagger-model-validator')

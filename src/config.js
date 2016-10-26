9713e155b50c6a289aa3a3ea00e52a26d03c4149
Avner Sorek <avner.sorek@gmail.com>
2016-10-26 10:41:40 +0300

    config values will be put into process.env

--- src/config.js
+++ src/config.js
@@ -12,6 +12,14 @@ function setConfigFileFolder(folder) {
   config = config
     .file('env-file', environmentConfigFile) // environment file is always of higher priority.
     .file('common-file', commonConfigFile);
+
+  putValuesIntoProcessEnv(config.get());
+}
+
+function putValuesIntoProcessEnv(config) {
+  for (let key in config) {
+    if (typeof config[key] === 'string' && !process.env[key]) process.env[key] = config[key];
+  }
 }
 
 function get(key) {
---
+++ test/unit/config.spec.js
@@ -0,0 +1,39 @@
+describe('config', function () {
+  const mockFs = require('mock-fs');
+  const _ = require('lodash');
+  const __ = require('hamjest');
+  var config;
+
+  const configFolder = 'some/ConfigFolder';
+  const commonConfigFile = { KEY1: 'coffee', SHARED_KEY: 'tea', ENV_KEY: 'water' };
+  const perEnvConfigFile = { KEY2: 'vodka', SHARED_KEY: 'beer' };
+  const envConfig = { KEY3: 'sprite', ENV_KEY: 'fanta' };
+
+  before(() => {
+    _.extend(process.env, envConfig);
+    mockFs({
+      [configFolder]: {
+        'common.json': JSON.stringify(commonConfigFile),
+        [ process.env.NODE_ENV + '.json' ]: JSON.stringify(perEnvConfigFile)
+      }
+    });
+    config = require('../../src').config;
+    config.setConfigFileFolder(configFolder);
+  });
+
+  it('should read values from config folder', () => {
+    __.assertThat(config.get('KEY1'), __.equalTo(commonConfigFile.KEY1));
+  });
+
+  it('should overide common values with per-environment values', () => {
+    __.assertThat(config.get('SHARED_KEY'), __.equalTo(perEnvConfigFile.SHARED_KEY));
+  });
+
+  it('should overide file values with env values', () => {
+    __.assertThat(config.get('ENV_KEY'), __.equalTo(envConfig.ENV_KEY));
+  });
+
+  it('should put values into process.env', () => {
+    __.assertThat(process.env.KEY2, __.equalTo(perEnvConfigFile.KEY2));
+  });
+});

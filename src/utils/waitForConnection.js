a5abb5fdbff7b2e07ef37024c1215b81fe889681
David Virtser <david@dorbel.com>
2016-10-31 17:09:09 +0200

    adding port support to waitForConnection

--- src/utils/waitForConnection.js
+++ src/utils/waitForConnection.js
@@ -3,8 +3,8 @@ const net = require('net');
 const url = require('url');
 const logger = require('../logger').getLogger(module);
 
-const RETRY_PERIOD_MS = 3000;
-const DEFAULT_RETRIES = 6;
+const RETRY_PERIOD_MS = 6000;
+const DEFAULT_RETRIES = 10;
 
 function waitForConnection(params, retries) {
   if (retries === undefined) {
@@ -18,14 +18,14 @@ function waitForConnection(params, retries) {
     }
 
     connect(params)
-    .once('connect', () => {
-      logger.info({ params }, 'Connection available');
-      resolve(true);
-    })
-    .once('error', () => {
-      logger.error({ retries, params }, 'Failed to connect, will retry');
-      setTimeout(() => resolve(waitForConnection(params, --retries)), RETRY_PERIOD_MS);
-    });
+      .once('connect', () => {
+        logger.info({ params }, 'Connection available');
+        resolve(true);
+      })
+      .once('error', () => {
+        logger.error({ retries, params }, 'Failed to connect, will retry');
+        setTimeout(() => resolve(waitForConnection(params, --retries)), RETRY_PERIOD_MS);
+      });
 
   });
 }
@@ -33,12 +33,23 @@ function waitForConnection(params, retries) {
 function connect(params) {
   if (params.port && params.host) {
     return net.connect(params.port, params.host);
-  }
-  else if (params.path) {
+  } else if (params.path) {
     let parsedPath = url.parse(params.path);
+
+    // Adding default ports in case they were not specified.
+    if (!parsedPath.port) {
+      switch (parsedPath.protocol) {
+        case 'http:':
+          parsedPath.port = 80;
+          break;
+        case 'https:':
+          parsedPath.port = 443;
+          break;
+      }
+    }
+
     return net.connect(parsedPath.port, parsedPath.hostname);
-  }
-  else {
+  } else {
     throw new Error('No connection params specificied');
   }
 }

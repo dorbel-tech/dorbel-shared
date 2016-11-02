1d09c872e11512a0bca575d367f44a3844c5a90f
David Virtser <david@dorbel.com>
2016-11-02 17:28:20 +0200

    adding shared eslint #6

--- src/koa-middleware/swagger-model-validator.js
+++ src/koa-middleware/swagger-model-validator.js
@@ -6,20 +6,27 @@ var validator = new Validator();
 function getMiddleware() {
   return function* validateSwaggerSchema(next) {
     const parameters = _.get(this, ['fleek', 'routeConfig', 'details', 'parameters']);
-    const bodyParamDef = _.find(parameters, { name: 'body', in: 'body' });
+    const bodyParamDef = _.find(parameters, {
+      name: 'body',
+      in: 'body'
+    });
     const body = _.get(this, ['request', 'body']);
     let error;
 
     if (bodyParamDef && bodyParamDef.schema) {
-      if (!body) error = 'missing body';
-      else error = validator.validate(this.request.body, bodyParamDef.schema);
+      if (!body) {
+        error = 'missing body';
+      } else {
+        error = validator.validate(this.request.body, bodyParamDef.schema);
+      }
     }
 
     if (error && !error.valid) {
       this.status = 400;
       this.response.body = conformToFleekError(error);
+    } else {
+      yield next;
     }
-    else yield next;
   };
 }
 

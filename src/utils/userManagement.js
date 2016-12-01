09fe12a7b46039e1b99647aafc3599556f4cb5fd
David Virtser <david@dorbel.com>
2016-12-01 18:13:56 +0200

    adding caching of auth0 user detail and token. #20

--- src/utils/userManagement.js
+++ src/utils/userManagement.js
@@ -5,30 +5,49 @@ const config = require('../config');
 const logger = require('../logger').getLogger(module);
 const ManagementClient = require('auth0').ManagementClient;
 const request = require('request-promise');
+const redis = require('redis');
+const bluebird = require('bluebird'); bluebird.promisifyAll(redis.RedisClient.prototype);
+const ONE_HOUR = 60 * 60;
+const ONE_DAY = ONE_HOUR * 24;
 
 // TODO: Implement user details caching.
 function getUserDetails(uuid) {
-  return getApiToken()
-    .then(token => {
-      return new ManagementClient({
-        token: token,
-        domain: config.get('AUTH0_DOMAIN')
-      });
-    })
-    .then(auth0 => {
-      return auth0.getUsers({
-        fields: 'name,email,user_metadata,app_metadata', // User details field names to get from API.
-        q: 'app_metadata.dorbel_user_id: ' + uuid // Query to get users by app metadata dorbel user id.
-      });
-    })
-    .then(user => {
-      logger.debug(user);
-      return user;
+  const cache = redis.createClient(6379, config.get('REDIS_HOST'));
+  const cacheKeyName = 'auth0_users';
+
+  return cache.hgetAsync(cacheKeyName, uuid)
+    .then(result => {
+      if (result) {
+        logger.info(cacheKeyName, 'got value from cache');
+        return JSON.parse(result);
+      } else {
+        logger.info(cacheKeyName, 'was missing in cache');
+        return getApiToken()
+          .then(token => {
+            return new ManagementClient({
+              token: token,
+              domain: config.get('AUTH0_DOMAIN')
+            });
+          })
+          .then(auth0 => {
+            return auth0.getUsers({
+              fields: 'name,email,user_metadata,app_metadata', // User details field names to get from API.
+              q: 'app_metadata.dorbel_user_id: ' + uuid // Query to get users by app metadata dorbel user id.
+            });
+          })
+          .then(user => {
+            cache.hset(cacheKeyName, uuid, JSON.stringify(user));
+            logger.debug(user);
+            return user;
+          });
+      }
     });
 }
 
 // TODO: Implement token caching and define expiration.
 function getApiToken() {
+  const cache = redis.createClient(6379, config.get('REDIS_HOST'));
+  const cacheKeyName = 'auth0_management_api_token';
   const authDomain = 'https://' + config.get('AUTH0_DOMAIN');
   const options = {
     method: 'POST',
@@ -42,9 +61,19 @@ function getApiToken() {
     json: true
   };
 
-  return request(options)
+  return cache.getAsync(cacheKeyName)
     .then(result => {
-      return result.access_token;
+      if (result) {
+        logger.info(cacheKeyName, 'got value from cache');
+        return result;
+      } else {
+        logger.info(cacheKeyName, 'was missing in cache');
+        return request(options)
+          .then(result => {
+            cache.setex(cacheKeyName, ONE_DAY, result.access_token);
+            return result.access_token;
+          });
+      }
     });
 }
 

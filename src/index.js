bf9062d86ff3bf934de6e9fec24e0063c6bcac28
David Virtser <david@dorbel.com>
2017-01-05 14:30:13 +0200

    moved normalizePhone to generics

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ module.exports = {
     waitForConnection: require('./utils/waitForConnection'),
     messageBus: require('./utils/messageBus'),
     userManagement: require('./utils/userManagement'),
-    analytics: require('./utils/analytics')
+    analytics: require('./utils/analytics'),
+    generic: require('./utils/generic'),
   }
 };
---
+++ src/utils/generic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Fix phone number by removing trailing zero and special characters.
+function normalizePhone(phone) {
+  if (phone.startsWith('0')) {
+    return '+972' + phone.substring(1).replace(/[-+()]/g, ''); 
+  } else {
+    return phone;
+  }
+}
+
+module.exports = {
+  normalizePhone
+};

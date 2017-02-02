7efb5a716f092134939e7b0d513f096b959cf650
Idan Langer <lxngxr@gmail.com>
2017-02-02 19:21:08 +0200

    Think i'm done. will validate on sunday.

--- src/helpers/cache.js
+++ src/helpers/cache.js
@@ -4,49 +4,71 @@
 const config = require('../config');
 const redis = require('redis');
 const bluebird = require('bluebird'); bluebird.promisifyAll(redis.RedisClient.prototype);
-let cacheInstance = null;
+const logger = require('../logger').getLogger(module);
 
-// Singleton cache class to get Redis client.
-class Cache {
-  constructor() {
-    if (!cacheInstance) { 
-      if (!config.get('REDIS_HOST')) { throw new Error('You need to define REDIS_HOST environment variable!'); }      
-      this.client = redis.createClient(6379, config.get('REDIS_HOST')); 
-      cacheInstance = this;
-    }    
-    return cacheInstance;
+if (!config.get('REDIS_HOST')) { throw new Error('You need to define REDIS_HOST environment variable!'); }
+
+const RECONNECT_REDIS_CLIENT_AFTER_FAIL_MS = 60 * 1000;
+
+let isRedisConnected = false;
+
+let cache = redis.createClient(6379, config.get('REDIS_HOST'), {
+  retry_strategy: (options) => {
+    options.toString();
+    return RECONNECT_REDIS_CLIENT_AFTER_FAIL_MS;
   }
+}).on('end', () => {
+  isRedisConnected = false;
+  logRedisDown();
+}).on('connect', () => {
+  isRedisConnected = true;
+  logger.info('cache helper: connected to Redis');
+}).on('reconnecting', () => {
+  logger.info('cache helper: trying to reconnect to redis');
+});
+
+function logRedisDown() {
+  let errorMessage =
+    `cache helper: could not connect to Redis. Will retry in ${RECONNECT_REDIS_CLIENT_AFTER_FAIL_MS / 1000} seconds`;
+  logger.error(errorMessage);
 }
 
+
 function getKey(cacheKeyName) {
-  let cache = new Cache();
-  return cache.client.getAsync(cacheKeyName).then(val => { return val; });
+  return isRedisConnected ?
+    cache.client.getAsync(cacheKeyName).then(val => { return val; })
+    : new Promise((resolve) => { resolve(); });
 }
 
 function setKey(cacheKeyName, value, expInSeconds) {
   if (value) {
-    let cache = new Cache();
-    if (expInSeconds) {
-      cache.client.setexAsync(cacheKeyName, expInSeconds, value);
-    } else {
-      cache.client.setAsync(cacheKeyName, value);
+    if (isRedisConnected) {
+      if (expInSeconds) {
+        cache.client.setexAsync(cacheKeyName, expInSeconds, value);
+      }
+      else {
+        cache.client.setAsync(cacheKeyName, value);
+      }
     }
-  } else {
+  }
+  else {
     throw new Error(`You are trying to cache undefined value to key ${cacheKeyName}!`);
   }
 }
 
 // Get global auth0 user from cache hash of users by uuid in Redis. 
-function getHashKey(hashName, hashKey){
-  let cache = new Cache();
-  return cache.client.hgetAsync(hashName, hashKey).then(val => { return val; });
+function getHashKey(hashName, hashKey) {
+  return isRedisConnected ?
+    cache.client.hgetAsync(hashName, hashKey).then(val => { return val; })
+    : new Promise((resolve) => { resolve(); });
 }
 
 // Update global auth0 user cache hash of users by uuid in Redis. 
 function setHashKey(hashName, hashKey, value) {
   if (value) {
-    let cache = new Cache();
-    cache.client.hsetAsync(hashName, hashKey, value);
+    if (isRedisConnected) {
+      cache.client.hsetAsync(hashName, hashKey, value);
+    }
   } else {
     throw new Error(`You are trying to cache undefined value to hash ${hashName} with key ${hashKey}!`);
   }

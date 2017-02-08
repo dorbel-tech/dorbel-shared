058ccf00fa400cf6bd66db4d3b12039d198c40af
David Virtser <poison77@gmail.com>
2017-02-08 14:59:55 +0200

    Multiple features (#43)
    * Remove health checks from request log
    
    * dont remove zero from phone https://github.com/dorbel-tech/dorbel-app/issues/438

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -2,11 +2,7 @@
 
 // Fix phone number by removing trailing zero and special characters.
 function normalizePhone(phone) {
-  if (phone.startsWith('0')) {
-    return phone.substring(1).replace(/[-+()]/g, ''); 
-  } else {
-    return phone;
-  }
+  return phone.replace(/[-+()]/g, ''); 
 }
 
 module.exports = {

499414c0a8098a22a340900e57712bae7b534cc7
David Virtser <david@dorbel.com>
2017-02-08 14:33:55 +0200

    Remove health checks from request log

--- src/koa-middleware/request-logger.js
+++ src/koa-middleware/request-logger.js
@@ -5,10 +5,14 @@ function getMiddleWare() {
   const logger = require('../logger').getLogger(module);
 
   return function* (next) {
-    var start = new Date;
+    let start = new Date;
     yield next;
-    var ms = new Date - start;
-    logger.info({ method: this.method, path: this.url, statusCode: this.status, duration: ms }, 'Response');
+    let ms = new Date - start;
+
+    // Don't report logs for health check requests.
+    if(this.url && (this.url.indexOf('/health') !== -1)) {
+      logger.info({ method: this.method, path: this.url, statusCode: this.status, duration: ms }, 'Response');
+    }
   };
 }
 

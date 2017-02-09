371fab78ba39700e488bc4575369cadc348983f5
Idan Langer <lxngxr@users.noreply.github.com>
2017-02-09 18:40:38 +0200

    Feature/sentry.io - forgot to remove testing line... :/ (#46)
    * integrate sentry.io
    
    * remove testing line

--- src/utils/sentryReporter.js
+++ src/utils/sentryReporter.js
@@ -6,7 +6,8 @@ const Raven = require('raven');
 const isDevelop = process.env.NODE_ENV === 'development';
 
 if (!isDevelop) {
-  const dsn = config.get('SENTRY_DSN') || 'https://1d5f4dd3c2204c3f99b619bf61435a8c:6fc5debcf13949f09ab8cb564dfe41d8@sentry.io/137086';
+  const dsn = config.get('SENTRY_DSN');
+
   if (dsn) {
     Raven.config(dsn).install();
   }

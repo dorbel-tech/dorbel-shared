c3c4c48051dcb6216fce44cafb0de27112961a57
David Virtser <david@dorbel.com>
2017-02-15 11:37:58 +0200

    bug fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # A script to release a new version of dorbel-shared module.
 
-echo "Starting release of ${VERSION}."
+echo "Starting release."
 
 echo -n "Please describe what was changed in this release: "
 read MSG
 
 # Change version in all npm package files
-npm version patch -m $MSG --git-tag-version
+npm version patch -m $(MSG) --git-tag-version
 
 # Release application
 git push --follow-tags

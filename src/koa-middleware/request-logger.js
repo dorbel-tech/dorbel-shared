0f2b9086391a23494d84d67242e8e50956d0b4ee
Avner Sorek <avnersorek@users.noreply.github.com>
2017-02-21 10:49:29 +0200

    add request id to request logger (#50)

--- src/koa-middleware/request-logger.js
+++ src/koa-middleware/request-logger.js
@@ -1,19 +1,33 @@
+'use strict';
+const uuidV4 = require('uuid/v4');
+const REQUEST_ID_HEADER = 'x-request-id';
+
 // All request logging middleware for koa server.
 // Reports all logs to logger.
-'use strict';
 function getMiddleWare() {
   const logger = require('../logger').getLogger(module);
 
   return function* (next) {
-    let start = new Date;
-    yield next;
-    let ms = new Date - start;
-
     // Don't report logs for health check requests.
-    if(this.url && this.url.indexOf('/health') === -1) {
-      logger.info({ method: this.method, path: this.url, statusCode: this.status, duration: ms }, 'Response');
+    if (!this.url || this.url.indexOf('/health') > -1) {
+      return yield next;
     }
+
+    const requestId = getOrSetRequestId(this.request.headers);
+    const start = new Date;
+    logger.trace({ method: this.method, path: this.url, requestId }, 'Request');
+
+    yield next;
+
+    const ms = new Date - start;
+    logger.info({ method: this.method, path: this.url, statusCode: this.status, duration: ms, requestId }, 'Response');
   };
+}
+
+function getOrSetRequestId(headers) {
+  // request ID might already be set on this request - if the request was proxied
+  headers[REQUEST_ID_HEADER] = headers[REQUEST_ID_HEADER] || uuidV4();
+  return headers[REQUEST_ID_HEADER];
 }
 
 module.exports = getMiddleWare;

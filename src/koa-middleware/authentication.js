bd5980e7d3bc97f69b9dade2e90b9fb86e0600c9
Avner Sorek <avnersorek@users.noreply.github.com>
2017-03-01 11:41:50 +0200

    Feature/more unit tests (#56)
    * adding test for authentication middleware
    
    * added test for error handler middleware
    
    * adding test for optional auth middleware

--- src/koa-middleware/authentication.js
+++ src/koa-middleware/authentication.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function* authenticate(next) {
-  const user = JSON.parse(this.request.headers['x-user-profile']);
+  let user;
+  const profileHeader = this.request.headers['x-user-profile'];
+
+  try {
+    user = profileHeader && JSON.parse(profileHeader);
+  } catch (ex) {
+    // nothing here
+  }
+
   if (user && user.id) {
     this.request.user = user;
     yield next;
---
+++ test/middleware/error-handler.spec.js
@@ -0,0 +1,62 @@
+'use strict';
+const __ = require('hamjest');
+const sinon = require('sinon');
+const mockRequire = require('mock-require');
+
+describe('middleware - error-handler', function () {
+  let newRelicMock, appMock, loggerMock, middleware;
+
+  before(function() {
+    newRelicMock = {
+      noticeError: sinon.spy()
+    };
+    appMock = {
+      emit: sinon.spy()
+    };
+    loggerMock = {
+      error: sinon.spy()
+    };
+    mockRequire('../../src/utils/newrelic', { init: () => newRelicMock });
+    mockRequire('../../src/logger', { getLogger: () => loggerMock });
+    middleware = mockRequire.reRequire('../../src/koa-middleware/error-handler')();
+  });
+
+  after(() => mockRequire.stopAll());
+
+  function * handleErrors(next) {
+    const context = { app: appMock };
+    yield middleware.bind(context)(next);
+    return context;
+  }
+
+  it('should continue flow if no error is thrown', function * () {
+    const next = sinon.spy(cb => cb());
+    const context = yield handleErrors(next);
+    __.assertThat(next.called, __.is(true));
+    __.assertThat(context.body, __.is(__.undefined()));
+    __.assertThat(context.status, __.is(__.undefined()));
+  });
+
+  it('should stop flow if error is thrown', function * () {
+    const error = { status: 980, message: 'bad request', stack: 'my stack' };
+    const next = sinon.stub().throws(error);
+    const context = yield handleErrors(next);
+    __.assertThat(context.body, __.is(error.message));
+    __.assertThat(context.status, __.is(error.status));
+    __.assertThat(appMock.emit.calledWith('error', error, context), __.is(true));
+    __.assertThat(newRelicMock.noticeError.calledWith(error), __.is(true));
+    __.assertThat(loggerMock.error.calledWith(error.stack, 'Server Error'), __.is(true));
+  });
+
+  it('should stop flow if no stack and status code', function * () {
+    const error = { message: 'general error' };
+    const next = sinon.stub().throws(error);
+    const context = yield handleErrors(next);
+    __.assertThat(context.body, __.is(error.message));
+    __.assertThat(context.status, __.is(500));
+    __.assertThat(appMock.emit.calledWith('error', error, context), __.is(true));
+    __.assertThat(newRelicMock.noticeError.calledWith(error), __.is(true));
+    __.assertThat(loggerMock.error.calledWith(error, 'Server Error'), __.is(true));
+  });
+
+});

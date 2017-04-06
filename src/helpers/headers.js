b28e8a6ed79dbbf37093c3e8c72089de153c6cd3
David Virtser <david@dorbel.com>
2017-04-06 17:54:44 +0300

    added more options

--- src/helpers/headers.js
+++ src/helpers/headers.js
@@ -1,18 +1,23 @@
 'use strict';
 
-function setPublicCacheHeader(response, MAX_AGE) {
+function setCacheHeader(response, MAX_AGE) {
   response.set('Cache-Control', 'max-age=' + MAX_AGE);
 }
 
-function setPrivateCacheHeader(request, response, MAX_AGE) {
+function setNoCacheHeader(response) {
+  response.set('Cache-Control', 'no-cache');
+}
+
+function setUserConditionalCacheHeader(request, response, MAX_AGE) {
   if (!request.user) {
-    setPublicCacheHeader(response, MAX_AGE);
+    setCacheHeader(response, MAX_AGE);
   } else {
-    response.set('Cache-Control', 'no-cache');
+    setNoCacheHeader(response);
   }  
 }
 
 module.exports = {
-  setPublicCacheHeader,
-  setPrivateCacheHeader
+  setCacheHeader,
+  setNoCacheHeader,
+  setUserConditionalCacheHeader
 };

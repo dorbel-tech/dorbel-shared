65b1c9538b2b008ac794367ce45d7098fa68c65a
David Virtser <david@dorbel.com>
2017-04-19 15:46:59 +0300

    Add info to logs for better bug handling #70

--- src/koa-middleware/error-handler.js
+++ src/koa-middleware/error-handler.js
@@ -1,6 +1,7 @@
 // Error handling middleware for koa server.
 // Reports error to logger.
 'use strict';
+
 function getMiddleWare() {
   const newrelic = require('../utils/newrelic').init();
   const logger = require('../logger').getLogger(module);
@@ -11,10 +12,24 @@ function getMiddleWare() {
     } catch (err) {
       this.status = err.status || 500;
       this.body = err.message;
+
       this.app.emit('error', err, this);
-      if (newrelic) { newrelic.noticeError(err); }
+      if (newrelic) {  newrelic.noticeError(err); }
+
       const requestId = this.request.headers['x-request-id'];
-      logger.error({ err, requestId }, 'Server Error');
+      let headers = undefined;
+      if (err.message === 'The header content contains invalid characters') {
+        headers = this.request.headers;
+      }
+
+      logger.error({
+        stack: err.stack,
+        method: this.method,
+        path: this.url,
+        statusCode: this.status,
+        requestId,
+        headers
+      }, err.message);
     }
   };
 }

5d6a190b9e05fbdea1e6b38603dfe367c7e14240
Idan Langer <lxngxr@users.noreply.github.com>
2017-04-20 18:21:11 +0300

    add tenant_profile to publicProfile (#71)
    * add tenant_profile to publicProfile
    
    * add tenant_profile to publicProfile 
    * renamed facebook_link to facebook_url for consistency (will update front-gateway too)
    * fallback to FB login only if tenant profile not set
    
    ** only merge when front gateway PR is approved **
    
    * publicProfile
    
    * fix
    
    * done
    
    * small fix

--- src/utils/userManagement.js
+++ src/utils/userManagement.js
@@ -88,22 +88,25 @@ function getPublicProfile(user_uuid) {
   }
 
   return getUserDetails(user_uuid)
-  .then(user => {
-    const publicProfile = {
-      email : _.get(user, 'user_metadata.email') || user.email,
-      first_name: _.get(user, 'user_metadata.first_name') || user.given_name,
-      last_name: _.get(user, 'user_metadata.last_name') || user.family_name,
-      phone: _.get(user, 'user_metadata.phone'),
-      picture: user.picture,
-      role: _.get(user, 'app_metadata.role')
-    };
+    .then(user => {
+      const publicProfile = {
+        email: _.get(user, 'user_metadata.email') || user.email,
+        first_name: _.get(user, 'user_metadata.first_name') || user.given_name,
+        last_name: _.get(user, 'user_metadata.last_name') || user.family_name,
+        phone: _.get(user, 'user_metadata.phone'),
+        picture: user.picture,
+        tenant_profile: _.get(user, 'user_metadata.tenant_profile')
+      };
 
-    if (_.get(user, 'identities[0].provider') === 'facebook') {
-      publicProfile.facebook_link = user.link;
-    }
+      if (!publicProfile.tenant_profile) {
+        publicProfile.tenant_profile = {};
+        if (_.get(user, 'identities[0].provider') === 'facebook') {
+          publicProfile.tenant_profile.facebook_url = user.link;
+        }
+      }
 
-    return publicProfile;
-  });
+      return publicProfile;
+    });
 }
 
 function getApiToken() {

8166d29814b4368ff47e5ec2fde2df4a6c77b9a2
Avner Sorek <avnersorek@users.noreply.github.com>
2017-04-27 18:34:50 +0300

    get public profile by email (#76)

--- src/utils/userManagement.js
+++ src/utils/userManagement.js
@@ -108,26 +108,31 @@ function getPublicProfile(user_uuid) {
     throw new Error('Cant get public user profile. Supplied user_uuid was undefined!');
   }
 
-  return getUserDetails(user_uuid)
-    .then(user => {
-      const publicProfile = {
-        email: _.get(user, 'user_metadata.email') || user.email,
-        first_name: _.get(user, 'user_metadata.first_name') || user.given_name,
-        last_name: _.get(user, 'user_metadata.last_name') || user.family_name,
-        phone: _.get(user, 'user_metadata.phone'),
-        picture: user.picture,
-        tenant_profile: _.get(user, 'user_metadata.tenant_profile')
-      };
+  return getUserDetails(user_uuid).then(normalizePublicProfile);
+}
 
-      if (!publicProfile.tenant_profile) {
-        publicProfile.tenant_profile = {};
-        if (_.get(user, 'identities[0].provider') === 'facebook') {
-          publicProfile.tenant_profile.facebook_url = user.link;
-        }
-      }
+function getPublicProfileByEmail(email) {
+  return getUserDetailsByEmail(email).then(normalizePublicProfile);
+}
 
-      return publicProfile;
-    });
+function normalizePublicProfile(user) {
+  const publicProfile = {
+    email: _.get(user, 'user_metadata.email') || user.email,
+    first_name: _.get(user, 'user_metadata.first_name') || user.given_name,
+    last_name: _.get(user, 'user_metadata.last_name') || user.family_name,
+    phone: _.get(user, 'user_metadata.phone'),
+    picture: user.picture,
+    tenant_profile: _.get(user, 'user_metadata.tenant_profile')
+  };
+
+  if (!publicProfile.tenant_profile) {
+    publicProfile.tenant_profile = {};
+    if (_.get(user, 'identities[0].provider') === 'facebook') {
+      publicProfile.tenant_profile.facebook_url = user.link;
+    }
+  }
+
+  return publicProfile;
 }
 
 function getApiToken() {
@@ -253,5 +258,6 @@ module.exports = {
   parseAuthToken,
   getProfileFromIdToken,
   getPublicProfile,
+  getPublicProfileByEmail,
   isUserAdmin
 };

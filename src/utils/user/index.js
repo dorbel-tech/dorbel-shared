686463c77181fa2c48c31d7955af3eaea9785378
David Virtser <david@dorbel.com>
2017-05-08 15:08:38 +0300

    bug fix

--- src/utils/user/index.js
+++ src/utils/user/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  helpers: require('helpers'),
-  management: require('management'),
-  permissions: require('permissions'),
+  helpers: require('./helpers'),
+  management: require('./management'),
+  permissions: require('./permissions'),
 };

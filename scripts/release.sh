71d7a6cd6277ec48d818babcc12bab71f008bdab
David Virtser <david@dorbel.com>
2017-07-02 12:04:00 +0300

    script improvement to allow release on master only

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # A script to release a new version of dorbel-shared module.
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $BRANCH != "master" ]]; then
+  echo "Sorry, releases allowed on master branch only!"
+  exit 1
+fi
+
+
 echo "Starting release."
 
 echo -n "Please describe what was changed in this release: "

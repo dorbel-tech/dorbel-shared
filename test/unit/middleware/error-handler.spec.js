175e35d74a862f5a4fd0f0c50b87c2a754131cf2
Idan Langer <lxngxr@gmail.com>
2017-07-26 14:51:22 +0300

    Added tests

--- test/unit/middleware/error-handler.spec.js
+++ test/unit/middleware/error-handler.spec.js
@@ -89,4 +89,31 @@ describe('middleware - error-handler', function () {
     __.assertThat(loggerMock.error.args[0][0], __.hasProperties({ error: error, requestId }));
   });
 
+  it('should return 400 error and return the errors in the response body for SequelizeValidationErrors', function* () {
+    const error = { message: 'general error', name: 'SequelizeValidationError', errors: ['mockField is required'] };
+    const next = sinon.stub().throws(error);
+    const context = yield handleErrors(next);
+    __.assertThat(context.body, __.is(error.errors));
+    __.assertThat(context.status, __.is(400));
+    __.assertThat(appMock.emit.calledWith('error', error, context), __.is(true));
+    __.assertThat(newRelicMock.noticeError.calledWith(error), __.is(true));
+    __.assertThat(loggerMock.error.args[0], __.contains(
+      __.hasProperties({ error }),
+      __.is(error.message)
+    ));
+  });
+
+  it('should return 500 error status and \'Internal error\' in the response body for general Sequelize errors', function* () {
+    const error = { message: 'general error', name: 'SequelizeSomethingSomething', errors: ['mockField is required'] };
+    const next = sinon.stub().throws(error);
+    const context = yield handleErrors(next);
+    __.assertThat(context.body, __.is('Internal error'));
+    __.assertThat(context.status, __.is(500));
+    __.assertThat(appMock.emit.calledWith('error', error, context), __.is(true));
+    __.assertThat(newRelicMock.noticeError.calledWith(error), __.is(true));
+    __.assertThat(loggerMock.error.args[0], __.contains(
+      __.hasProperties({ error }),
+      __.is(error.message)
+    ));
+  });
 });
